344d1907aef16b4f354e90f1882add50b13a8bb1
doris <qianyh0806@163.com>
2017-03-02 16:25:14 +0800

    finish testrun

--- Untility/testrun.py
+++ Untility/testrun.py
@@ -1,17 +1,15 @@
 #! usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import unittest
+
 __author__ = "Doris"
 
-import unittest
-from TestCases.case_login import LoginTest
+suit = unittest.defaultTestLoader.discover("TestCases", pattern='case_*.py')
 
-#suit = unittest.defaultTestLoader.discover("TestCases", pattern='case_*.py')
-testunit = unittest.TestSuite()
-testunit.addTest(unittest.makeSuite(LoginTest))
 
-#if __name__ == '_main_':
-runner = unittest.TextTestRunner()
-print(3)
-#runner.run()
-runner.run(testunit)
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(suit)
+
+
